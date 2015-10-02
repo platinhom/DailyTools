91c43ed23ad986188b6d3fb06c138fe7516d0f2d
platinhom <zhaozxcpu@hotmail.com>
2015-10-02 06:03:53 -0400

    eses_atomarea

--- scripts/ESES_AtomicArea.py
+++ scripts/ESES_AtomicArea.py
@@ -2,12 +2,11 @@
 # -*- coding: utf8 -*-
 # Author: Platinhom; Last Updated: 2015-10-1
 
-# Calculate each element surface area by ESES(MS_Intersection) and also match the atomic area results to the pqr file.
-# Usage: python ESES_AtomicArea.py file.pqr
+# Summary each atom type surface area from the pqrta file.
+# Usage: python ESES_AtomicArea.py file.pqrta gaff
 #
-# Need: MS_Intersection (partition version), AmberTools
-# Note: Only for PQR format input.
-# Custom: ESES parameters.
+# Note: Only for PQRTA format input.
+# Custom: ESES parameters, atom type set
 
 import os,sys
 
@@ -17,95 +16,53 @@ probe=1.4
 grid=0.2
 solbuffer=4.0
 
+# 5 sets of atom type
+gaff=["br", "c", "c1", "c2", "c3", "ca", "cc", "cd", "ce", "cf", "cg", "cl", "cp", "cx", "cy", "f", "h1", "h2", "h3", "h4", "h5", "ha", "hc", "hn", "ho", "hs", "i", "n", "n1", "n2", "n3", "na", "nb", "nc", "nd", "ne", "nf", "nh", "no", "o", "oh", "os", "p5", "s", "s4", "s6", "sh", "ss", "sy"]
+
+amber=["Br", "C", "C*", "CA", "CB", "CD", "CK", "Cl", "CM", "CN", "CQ", "CR", "CT", "CV", "CW", "CZ", "DU", "F", "H", "H1", "H2", "H3", "H4", "H5", "HA", "HC", "HO", "HS", "I", "N", "N*", "N1", "N2", "NA", "NB", "NC", "NO", "NT", "O", "O2", "OH", "OS", "OW", "P", "S", "SH", "SO"]
+
+sybyl=["Br", "C.1", "C.2", "C.3", "C.ar", "Cl", "F", "H", "I", "N.1", "N.2", "N.3", "N.am", "N.ar", "N.pl3", "O.2", "O.3", "P.3", "S.2", "S.3", "S.o", "S.o2"]
+
+bcc=["11", "12", "13", "14", "15", "16", "17", "21", "22", "23", "24", "25", "31", "32", "33", "42", "51", "52", "53", "71", "72", "73", "74", "91"]
+
+gas=["br", "c1", "c2", "c3", "cl", "f", "h", "i", "n1", "n2", "n3", "na", "o2", "o3", "os", "p", "s2", "s3", "so", "so2"]
+
 if (__name__ == '__main__'):
 	fname=sys.argv[1]
 	fnamelist=os.path.splitext(fname)
-	fxyzr=open(fnamelist[0]+".xyzr",'w')
+	if (fnamelist[1].lower()!='pqrta'):
+		print 'Input should be pqrta format!'
+		exit(1)
+	fnum=fnamelist[1].split('_')[0];
+	settype=[]
+	if (sys.argv[2].lower='gaff'):settype=gaff
+	elif (sys.argv[2].lower='amber'):settype=amber
+	elif (sys.argv[2].lower='sybyl'):settype=sybyl
+	elif (sys.argv[2].lower='bcc'):settype=bcc
+	elif (sys.argv[2].lower='gas'):settype=gas
+	else:
+		print 'Atom type set should be given, gaff, amber, sybyl, bcc or gas!'
+		exit(1)
+	areatype={}
+	for t in settype:
+		areatype[t]=0.0
 	fr=open(fname)
-	inlines=fr.readlines();
-	fr.close();
 
-	# All elements/types of input atoms, used in element area summary.
-	atomtypes=[];
-	# Write out the corresponding xyzr file.
-	for line in inlines:
+	for line in fr:
 		# Each atom
 		if (line[:4]=="ATOM" or line[:6]=="HETATM"):
-			# Atom element here
 			tmp=line.split();
-			element=tmp[-1].upper();
-			atomtypes.append(element);
-			# Extract x, y, z, r from pqr to xyzr file
-			radius="%10.5f" % float(line[62:70].strip());
-			xcoor="%10.5f" % float(line[30:38].strip());
-			ycoor="%10.5f" % float(line[38:46].strip());
-			zcoor="%10.5f" % float(line[46:54].strip());
-			xyzrstr=xcoor+ycoor+zcoor+radius+"\n";
-			fxyzr.write(xyzrstr);
-	fxyzr.close()
-	
-	# Use external ESES program to generate surface and calculate atom area
-	## So you have to put the ESES program in the same directory
-	# Output a "partition_area.txt" file saving atom area
-	#os.system('./MS_Intersection_Area '+fnamelist[0]+".xyzr "+str(probe)+" "+str(grid)+" "+str(solbuffer));
-	p=os.popen('./MS_Intersection '+fnamelist[0]+".xyzr "+str(probe)+" "+str(grid)+" "+str(solbuffer),'r')
-	totalArea="0"
-	totalVolume="0"
-	while 1:
-		line=p.readline();
-		if "area:" in line: totalArea=line.split(':')[1].split()[0]
-		if "volume:" in line: totalVolume=line.split(':')[1].split()[0]
-		if not line:break
+			atype=tmp[-2].strip();
+			aarea=float(tmp[-1].strip());
+			if (atype not in settype): settype.append(atype);
+			areatype[atype]=areatype.setdefault(atype,0.0)+aarea;
+	fr.close()
 
-	# Analyze output atom area file
-	fa=open("partition_area.txt")
-	atomareas=[];# tmp save atom area by atom number
-	typedefault=["H","C","N","O","F","S","P","CL","BR","I"];
-	typeareas={"H":0.0,"C":0.0,"N":0.0,"O":0.0,"F":0.0,"S":0.0,"P":0.0,"CL":0.0,"BR":0.0,"I":0.0};
-	atomnum=0;
-	for line in fa:
-		tmp=line.split();
-		atomarea="%12.6f" % float(tmp[1]);
-		atomareas.append(atomarea);
-		atype=atomtypes[atomnum];
-		typeareas[atype]=typeareas.setdefault(atype,0.0)+float(tmp[1]);
-		atomnum=atomnum+1;
-	fa.close()
-	
-	# Write out pqra file saving atom area
-	fwname=fnamelist[0]+"_area.pqra"
-	fw=open(fwname,'w')
-	
-	# Write the total area for each element.
-	## Notice that here just write out the default elements.
-	## If you want all elements, use "typeused" for iteration.
-	typeused=["H","C","N","O","F","S","P","CL","BR","I"];
-	for i in typeareas.iterkeys():
-		if i not in typeused:typeused.append(i);
-	
-	# For print out the atom area summary
-	outputelearea=fnamelist[0]+" Areas: "+totalArea+" Volumes: "+totalVolume+" ";
-	fw.write("REMARK  AREAS  "+totalArea+"\n");
-	fw.write("REMARK  VOLUMES  "+totalVolume+"\n");	
+	outputatype=fnum+' '
+	outputaarea=fnum+' '
+	for atype in settype:
+		outputatype=outputatype+atype+' '
+		outputaarea=outputaarea+str(areatype[atype])+" ";
+	print outputatype+' : '+outputaarea;
 
-	for element in typedefault:
-	# If you want all elements, need to comment the above line and uncomment the following line.
-	#for element in typeused:
-		fw.write("REMARK  AREA  "+"%2s"%element+"  "+"%20.6f"%typeareas.get(element,0.0)+"\n");
-		outputelearea=outputelearea+element+": "+str(typeareas[element])+" ";
-	print outputelearea
-	
-	fr=open(fname)
-	atomnum=0;
-	for line in fr:
-		if (line[:4]=="ATOM" or line[:6]=="HETATM"):
-			tmp=line.split();
-			element=tmp[-1].upper();
-			newline=line.strip('\n')+atomareas[atomnum]+"\n";
-			fw.write(newline);
-			atomnum=atomnum+1;
-		else:
-			fw.write(line);
-	fr.close();
-	fw.close()
 #end main			
