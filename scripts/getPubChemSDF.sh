190584430e5d4a1dfe7295c4373f14ab1bc19b8f
platinhom <zhaozxcpu@hotmail.com>
2015-11-26 17:11:37 -0500

    regular

--- scripts/getPubChemSDF.sh
+++ scripts/getPubChemSDF.sh
@@ -1,24 +1,25 @@
 #! /bin/bash
 #
 # File: getPubChemSDF.sh
-# Author: Hom, 2015.9.30
+# Author: Hom, 2015.11.26
 # Get the Pubchem compounds in 3D-sdf format by cid saved in a file.
 #
 # Usage: ./getPubChemSDF.sh cid.txt 
+# To get a single file: echo cid | ./getPubChemSDF.sh
 # 	$1: file contains Pubchem Compound ID (one number a line)
+#   $2: dir name to save result, default '.'
 
-NewDir=GETCOMPOUNDS
-mkdir $NewDir
+cidlist=`cat $1`
+if [ ! -z $2 ];then 
+NewDir=$2
+[ -d $NewDir ] || mkdir $NewDir
+cd $NewDir
+fi
 
-for fil in `cat $1`
+for fil in $cidlist
 do
 if [ ! -f ${fil}.sdf ];then
 	dat="https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/cid/${fil}/record/SDF/?response_type=save&record_type=3d"
 	wget $dat -O ${fil}.sdf
-	mv ${fil}.sdf $NewDir
-else
-	mv ${fil}.sdf $NewDir
 fi
 done
-
-
